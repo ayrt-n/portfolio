dd9c1f7728506ae2a4c6922d142cd10ca2d3705c
ayrton <ayrton.parkinson1@gmail.com>
2023-05-11 06:28:34 -0400

    Initial commit

---
+++ src/assets/images/file_types/fileIcons.js
@@ -0,0 +1,19 @@
+import css from './css.svg';
+import defaultIcon from './default.svg';
+import ico from './favicon.svg';
+import gitignore from './git_ignore.svg';
+import html from './html.svg';
+import md from './info.svg';
+import js from './javascript.svg';
+import rb from './ruby.svg';
+
+export const fileIcons = {
+  css,
+  defaultIcon,
+  ico,
+  gitignore,
+  html,
+  js,
+  md,
+  rb,
+}
