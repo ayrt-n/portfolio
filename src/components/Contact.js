a2687eca64dd2d7052936083e4d6ac245602769f
ayrton <ayrton.parkinson1@gmail.com>
2023-05-11 06:28:19 -0400

    Add outlet components

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      Contact
+    </div>
+  )
+}
+
+export default Contact;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      Portfolio
+    </div>
+  )
+}
+
+export default Portfolio;
