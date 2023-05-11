6660a8286403474303f55e69e8bc3e815a0389c3
ayrton <ayrton.parkinson1@gmail.com>
2023-05-11 06:27:46 -0400

    Add routes to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Dashboard />}>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
