060b18e65437bb218e2d4d88dc6941b9e13d8ca0
ayrton <ayrton.parkinson1@gmail.com>
2023-05-13 07:59:01 -0400

    Create no tabs component

---
+++ src/components/NoTabs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NoTabs() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      No tabs
+    </div>
+  );
+}
+
+export default NoTabs;
