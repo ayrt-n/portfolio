9f0c9edb66d01032a63af530c8fdc864518f75e0
ayrton <ayrton.parkinson1@gmail.com>
2023-05-13 18:37:24 -0400

    Add Home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      Home
+      <h1 className="text-5xl mb-3">
+        Hi! I'm <span className="text-pink">Ayrton!</span>
+      </h1>
+      <h2 className='text-3xl'>
+        A Full Stack Software Engineer from Canada
+        <span className="animate-blink">|</span>
+      </h2>
     </div>
   )
 }
