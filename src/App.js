64c6904f0b9f1d46e86bedce7e97c879e589b5be
ayrton <ayrton.parkinson1@gmail.com>
2023-05-13 18:36:38 -0400

    Remove routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import TabProvider from './contexts/tab/TabProvider';
 import Home from './components/Home';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
 
 function App() {
+  const tabs = [
+    { name: 'Home', type: 'file', extension: 'js', component: <Home /> },
+    { name: 'About', type: 'file', extension: 'js', component: <About /> },
+  ];
+
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <TabProvider initialTabs={tabs}>
+      <Dashboard />
+    </TabProvider>
   );
 }
 
