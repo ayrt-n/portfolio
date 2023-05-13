4c90a6dc58577f496311ed35cf8ce2ca586b8dd4
ayrton <ayrton.parkinson1@gmail.com>
2023-05-13 07:59:48 -0400

    Hook directory item up to tab context

--- src/components/sidebar/DirectoryItem.js
+++ src/components/sidebar/DirectoryItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import File from '../File';
 import useLevelContext from '../../hooks/useLevelContext';
+import useTabContext from '../../hooks/useTabContext';
 
-function DirectoryItem({ children }) {
+function DirectoryItem({ name, extension, pathname }) {
   const level = useLevelContext();
   const paddingLeft = `${level * 10}px`;
 
+  const tabs = useTabContext();
+  const handleClick = () => {
+    tabs.addTab({name, extension, pathname});
+  };
+
   return (
-    <div className="hover:bg-dark-400 py-1" style={{paddingLeft: paddingLeft}}>
-      {children}
-    </div>
+    <button onClick={handleClick} className="hover:bg-dark-400 py-1 font-medium block w-full" style={{paddingLeft: paddingLeft}}>
+      <File name={name} extension={extension} />
+    </button>
   );
 }
 
