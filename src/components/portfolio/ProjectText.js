c3d392a400d7087fd76d153d805c4c060d7c14aa
ayrton <ayrton.parkinson1@gmail.com>
2023-05-15 07:39:09 -0400

    Add github and live links

--- src/components/portfolio/ProjectText.js
+++ src/components/portfolio/ProjectText.js
@@ -6,7 +6,11 @@ function ProjectText({ title, technologies, description, githubLink, liveLink })
     <div className="max-w-[400px]">
       <MarkdownHeader headingLevel="2" text={title} className="text-3xl mb-3" />
       <p className="text-comment mb-3">{technologies}</p>
-      <p>{description}</p>
+      <p className="mb-3">{description}</p>
+      <div className="flex">
+        {githubLink && <a href={githubLink} target="_blank" rel="noreferrer" className="text-green hover:bg-green-highlight font-bold mr-4">[GitHub]</a>}
+        {liveLink && <a href={liveLink} target="_blank" rel="noreferrer" className="text-green hover:bg-green-highlight font-bold">[Live Preview]</a>}
+      </div>
     </div>
   );
 }
