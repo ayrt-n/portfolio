95155e4290ed60b7df00f6def4675fa9584b20e3
ayrton <ayrton.parkinson1@gmail.com>
2023-05-17 07:38:57 -0400

    Add vscode icon

--- src/components/NoTabs.js
+++ src/components/NoTabs.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import vsIcon from '../assets/images/vs_code_icon.svg';
 
 function NoTabs() {
   return (
-    <div className="flex flex-col justify-center items-center row-span-full">
-      No tabs
+    <div className="min-h-screen flex flex-col justify-center items-center row-span-full">
+      <div>
+        <img className="h-56 w-56 mb-6" src={vsIcon} alt="" />
+      </div>
     </div>
   );
 }
