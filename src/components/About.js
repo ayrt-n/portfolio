9feffd7674e1d28ea2c75dc62e2fbf44477de7f9
ayrton <ayrton.parkinson1@gmail.com>
2023-05-17 07:38:28 -0400

    Add download cv link

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,14 @@ function About() {
       <Section>
         <MarkdownHeader headingLevel="1" text="About" className="text-3xl md:text-5xl" />
         <LineBreak />
-        <Text>Hi! My name is Ayrton Parkinson. I am a full stack software engineer. I am currently pursuing my Bachelor of Computer Science at Carleton University.</Text>
-        <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <span className="text-green font-bold">Projects.js</span>.</Text>
+        <Text>Hi! My name is Ayrton Parkinson. I am a full stack software engineer and am currently pursuing my Bachelor of Computer Science at Carleton University.</Text>
+        <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <span className="text-green font-bold">Portfolio.js</span>.</Text>
         <Text>I am looking for positions as a backend or full stack software engineer. I am incredibly passionate about the work that I do and would love to chat about how I might be able to help the work that you do!</Text>
         <Text>For information on how to reach out, see <span className="font-bold text-green">Contact.md</span>.</Text>
+      </Section>
+
+      <Section>
+        <button className="font-bold text-green hover:bg-green-highlight">[Download Resume]</button>
       </Section>
 
       <Section>
