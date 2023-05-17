eeecf1f3ab7c82db54af6721a8e672a55ae2f9be
ayrton <ayrton.parkinson1@gmail.com>
2023-05-17 08:14:48 -0400

    Create link component to encapsulate styling / props

---
+++ src/components/Link.js
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import React from 'react';
+
+function Link({ as, className, children, targetBlank, ...props }) {
+  const LinkTag = as || 'a'
+
+  const defaultClasses = "text-blue hover:bg-blue-highlight underline"
+  const mergedClasses = classNames(defaultClasses, className);
+
+  const targetProps = targetBlank ? { target: '_blank', rel: 'noreferrer' } : {};
+
+  return (
+    <LinkTag className={mergedClasses} {...targetProps} {...props}>
+      {children}
+    </LinkTag>
+  );
+}
+
+export default Link;
