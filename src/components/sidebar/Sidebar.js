378a53a4f4e01ddbe0bfaa4f706988c4d242f301
ayrton <ayrton.parkinson1@gmail.com>
2023-05-17 07:39:09 -0400

    Set min-h-screen

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Sidebar({ open, children }) {
   return (
-    <div className={`bg-dark-600 group select-none sticky top-0 max-h-screen ${open ? "min-w-[300px]" : "min-w-[50px]"}`}>
+    <div className={`bg-dark-600 group select-none sticky top-0 min-h-screen ${open ? "min-w-[300px]" : "min-w-[50px]"}`}>
       <div className={`${!open && "hidden"}`}>
         {children}
       </div>
