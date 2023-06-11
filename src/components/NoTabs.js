8a0871d4a0f4f50113a2fbb39a5680ed03b09378
ayrton <ayrton.parkinson1@gmail.com>
2023-06-11 08:17:27 -0400

    Adjust image size on mobile

--- src/components/NoTabs.js
+++ src/components/NoTabs.js
@@ -5,7 +5,7 @@ import KeyboardIcon from './KeyboardIcon';
 function NoTabs() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center row-span-full">
-      <img className="h-60 mb-6" src={apIcon} alt="" />
+      <img className=" h-40 md:h-60 mb-6" src={apIcon} alt="" />
       <div className="text-white-50a font-medium tracking-wide hidden md:grid grid-cols-commands gap-3">
         <span className="text-right">Toggle Menu</span>
         <div className="flex gap-x-2 items-center">
