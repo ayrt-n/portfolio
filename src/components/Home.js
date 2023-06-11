8b47ae1e72e80f56105444c4c99cedf4d9ea92da
ayrton <ayrton.parkinson1@gmail.com>
2023-06-11 08:49:45 -0400

    Replace hr with line break component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LineBreak from './LineBreak';
 
 function Home() {
   return (
@@ -10,7 +11,7 @@ function Home() {
         A Full Stack Software Engineer from Canada
         <span className="animate-blink">|</span>
       </h2>
-      <hr className="border-dark-200 my-3" />
+      <LineBreak />
     </div>
   )
 }
