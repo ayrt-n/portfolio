f44b44390846d226fdacd992aee027132dde70a4
ayrton <ayrton.parkinson1@gmail.com>
2023-06-12 12:16:55 -0400

    Adjust About text

--- src/components/About.js
+++ src/components/About.js
@@ -14,9 +14,9 @@ function About() {
         <MarkdownHeader headingLevel="1" text="About" className="text-3xl md:text-5xl" />
         <LineBreak />
         <Text>Hi! My name is Ayrton Parkinson. I am a full stack software engineer and am currently pursuing my Bachelor of Computer Science at Carleton University.</Text>
-        <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <span className="font-bold">Portfolio.js</span>.</Text>
+        <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <span className="font-bold text-yellow">Portfolio.js</span>.</Text>
         <Text>I am looking for positions as a backend or full stack software engineer. I am incredibly passionate about the work that I do and would love to chat about how I might be able to help the work that you do!</Text>
-        <Text>For information on how to reach out, see <span className="font-bold">Contact.md</span>.</Text>
+        <Text>For information on how to reach out, see <span className="font-bold text-yellow">Contact.md</span>.</Text>
       </Section>
 
       <Section>
@@ -29,7 +29,7 @@ function About() {
         <MarkdownHeader headingLevel="2" text="Technologies" className="text-xl md:text-3xl" />
         <LineBreak />
         <Comment>
-          While these are some of the technologies I am the most comfortable with, these are just tools to get the job done! I am constantly looking to expand my toolkit and am open to working with and learning new technologies.
+          While these are some of the languages I am the most comfortable with, I am constantly looking to expand my toolkit and am open to working with and learning new skills!
         </Comment>
         <Text>
           <span className="font-bold text-pink">Languages and Frameworks:</span> Ruby, Ruby on Rails, GraphQL, SQL, React, JavaScript, HTML, CSS, Tailwind
