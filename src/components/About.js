69c87eb282c693cfcaa8b674bf515fed128a85a3
ayrton <ayrton.parkinson1@gmail.com>
2023-06-14 18:41:46 -0400

    Update about section

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
       <Section>
         <MarkdownHeader headingLevel="1" text="About" className="text-3xl md:text-5xl" />
         <LineBreak />
-        <Text>Hi! My name is Ayrton Parkinson. I am a full stack software engineer and am currently pursuing my Bachelor of Computer Science at Carleton University.</Text>
+        <Text>Hi! My name is Ayrton Parkinson. I am a self-taught full stack software engineer from Ontario, Canada.</Text>
         <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <span className="font-bold text-yellow">Portfolio.js</span>.</Text>
         <Text>I am looking for positions as a backend or full stack software engineer. I am incredibly passionate about the work that I do and would love to chat about how I might be able to help the work that you do!</Text>
         <Text>For information on how to reach out, see <span className="font-bold text-yellow">Contact.md</span>.</Text>
