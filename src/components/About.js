2cd0b5a9f53888505f6807f2ad36ee1a5b166c44
ayrton <ayrton.parkinson1@gmail.com>
2023-08-07 09:42:49 -0400

    Update About text

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ function About() {
       <Section>
         <MarkdownHeader headingLevel="1" text="About" className="text-3xl md:text-5xl" />
         <LineBreak />
-        <Text>Hi! My name is Ayrton Parkinson. I am a self-taught full stack software engineer from Ontario, Canada.</Text>
+        <Text>Hi! My name is Ayrton Parkinson. I am a full stack software engineer, currently pursuing my Bachelor of Science in Computer Science from Carleton University.</Text>
         <Text>While originally going to school for economics and finance, I started coding as a hobby through 2021 and from that point on, I was hooked. Since then I have been learning and building every opportunity I get. For some examples of recent work, see <TabLink className="font-bold text-yellow" tab={portfolio}>Portfolio.js</TabLink>.</Text>
         <Text>I am looking for positions as a backend or full stack software engineer. I am incredibly passionate about the work that I do and would love to chat about how I might be able to help the work that you do!</Text>
         <Text>For information on how to reach out, see <TabLink className="font-bold text-yellow" tab={contact}>Contact.md</TabLink>.</Text>
@@ -34,7 +34,7 @@ function About() {
           While these are some of the languages I am the most comfortable with, I am constantly looking to expand my toolkit and am open to working with and learning new skills!
         </Comment>
         <Text>
-          <span className="font-bold text-pink">Languages and Frameworks:</span> Ruby, Ruby on Rails, GraphQL, SQL, React, JavaScript, HTML, CSS, Tailwind
+          <span className="font-bold text-pink">Languages and Frameworks:</span> Ruby, Ruby on Rails, Python, GraphQL, SQL, React, JavaScript, HTML, CSS, Tailwind
         </Text>
         <Text>
           <span className="font-bold text-pink">Miscellaneous:</span> Git, Webpack, Object-Oriented Programming and Design, Test-Driven Development, Data Structures and Algorithms, Full Stack Web Development, REST APIs, Amazon S3
