260b4dc77872d98fb52da7db9ddc4af91a008197
ayrton <ayrton.parkinson1@gmail.com>
2023-11-02 07:52:21 -0400

    Remove gitignore from directory

--- src/utils/directory.js
+++ src/utils/directory.js
@@ -4,13 +4,11 @@ import About from '../components/About';
 import Contact from '../components/Contact';
 import IndexCSS from '../components/IndexCSS';
 import Portfolio from '../components/Portfolio';
-import GitIgnore from '../components/GitIgnore';
 
 const about = { name: 'About', type: 'file', extension: 'js', component: <About /> }
 const portfolio = { name: 'Portfolio', type: 'file', extension: 'js', component: <Portfolio /> }
 const home = { name: 'Home', type: 'file', extension: 'js', component: <Home /> }
 const index = { name: 'index', type: 'file', extension: 'css', component: <IndexCSS /> }
-const gitignore = { name: ' ', type: 'file', extension: 'gitignore', component: <GitIgnore /> }
 const contact = { name: 'Contact', type: 'file', extension: 'md', component: <Contact /> }
 
 const directory = [{
@@ -33,7 +31,6 @@ const directory = [{
         index,
       ]
     },
-    gitignore,
     contact,
   ]
 }];
@@ -43,7 +40,6 @@ export {
   portfolio,
   home,
   index,
-  gitignore,
   contact,
   directory
 };
